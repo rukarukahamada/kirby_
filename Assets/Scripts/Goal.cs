b13b61c14db90930483822cfed79030846a103fe
GTUNE2-01\ktc <>
2025-07-01 17:41:38 +0900

    タイトル・ゲームオーバー・ゲームクリア・ステージ、ゴール判定作成

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    // 触れたら切り替えたいシーン名
+    public string GAMECLEAR;
+    
+    // プレイヤーがブロックに触れた時の処理
+    private void OnCollisionEnter(Collision collision)
+    {
+        // プレイヤーとの接触を確認
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // シーンを切り替え
+            SceneManager.LoadScene("GAMECLEAR");
+        }
+    }
+}
